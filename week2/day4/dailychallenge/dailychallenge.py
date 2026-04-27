11a24033cfef5cbce240626ac7bd4aa891c55c95
Henri Safra <safrinha.safra@gmail.com>
2026-04-27 15:25:17 +0300

    Update dailychallenge.py

--- week2/day4/dailychallenge/dailychallenge.py
+++ week2/day4/dailychallenge/dailychallenge.py
@@ -1,9 +1,28 @@
-user_input = input("Enter words separated by commas: ")
+words_input = input("Enter words separated by commas: ")
 
-words_list = user_input.split(',')
+words_list = words_input.split(",")
 
 words_list.sort()
 
 result = ",".join(words_list)
 
 print(result)
+nter words separated by commas: henri, safra
+ safra,henri
+def longest_word(sentence):
+    words = sentence.split()
+    
+    longest = ""
+    
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+            
+    return longest
+
+print(longest_word("Margaret's toy is a pretty doll."))
+print(longest_word("A thing of beauty is a joy forever."))
+print(longest_word("Forgetfulness is by all means powerless!"))
+Margaret's
+forever.
+Forgetfulness
